f118f56a1fe7308b24962b6294f49f3decf39a94
Bribb0L <50684429+Bribb0L@users.noreply.github.com>
2022-12-21 03:23:12 -0300

    Add calculator ui with a display function

---
+++ script.js
@@ -0,0 +1,13 @@
+function display(input){
+    const output = document.querySelector('.output');
+    output.textContent = input;
+}
+
+display(4);
+
+
+/*     currentInput = Number(output['textContent']);
+    (currentInput === 0) 
+    ? output.textContent = input 
+    : output.textContent += input;
+    console.log(currentInput === 0)  */
